34e9ef5679735b49d533f144dc6b6f8d5cae3f6e
Mirboboyev Jasur <mirboboyevjasurjan@gmail.com>
2022-08-18 15:33:04 +0300

    map Cources

--- src/components/templates/Courses/Coureses.jsx
+++ src/components/templates/Courses/Coureses.jsx
@@ -1,9 +1,9 @@
 import "./courses.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import {AiFillHeart} from 'react-icons/ai';
-import {AiOutlineHeart} from 'react-icons/ai';
-import {FaAngleDoubleRight, FaAngleDoubleLeft} from 'react-icons/fa';
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FaAngleDoubleRight, FaAngleDoubleLeft } from "react-icons/fa";
 import course1 from "../../images/course-1-1.jpg";
 import course2 from "../../images/course-1-2.jpg";
 import course3 from "../../images/course-1-3.jpg";
@@ -19,9 +19,9 @@ import team6 from "../../images/team-1-2.jpg";
 
 const Courses = () => {
   const menuHandler = () => {
-    const element = document.querySelector('.like_icons1');
-    const element2 = document.querySelector('.like_icons2');
-    
+    const element = document.querySelector(".like_icons1");
+    const element2 = document.querySelector(".like_icons2");
+
     if (element.style.opacity === "1") {
       element.style.opacity = "0";
       element2.style.opacity = "1";
@@ -29,57 +29,58 @@ const Courses = () => {
       element.style.opacity = "1";
       element2.style.opacity = "0";
     }
-  }
+  };
 
-const courseCards =  [
-  {
-  "img": course1,
-  "courseCategory": "Development",
-  "byName": "Jasur",
-  "courseDetals":"New react bootcamp",
-  "hours":"10 Hours",
-  "lectures":"6 Lectures",
-  "price": "100$"
+  const createCourse = (img, tag, author, title, hours, lectures, price) => {
+    return { img, tag, author, title, hours, lectures, price };
+  };
 
-}
-]
+  const courseCards = [
+    createCourse(course1, "Development", "Nurlan Karshiev", "New React Bootcamp", "10 Hours", "6 Lectures", "100$" ),
+    createCourse(course2, "Development", "Nurlan Karshiev", "New React Bootcamp", "10 Hours", "6 Lectures", "100$" ),
+    createCourse(course3, "Development", "Nurlan Karshiev", "New React Bootcamp", "10 Hours", "6 Lectures", "100$" )
+  ];
   return (
-    
-
     <section className="course-one course-page">
-       <div className="container">
-          <div className="row">
-            
-            {courseCards.map((img, courseCategory, byName, courseDetals, hours, lectures, price  )=>{
-              <div className="col-lg-4">
+      <div className="container">
+        <div className="row">
+          {courseCards.map((card, index) => {
+            return (
+            <div className="col-lg-4">
               <div className="course-one__single">
                 <div className="course-one__image">
-                  <img src={img} alt="" />
+                  <img src={card.img} alt="" />
                   <i className="like_icons">
-                    <AiOutlineHeart onClick={menuHandler} className="like_icons1" />
-                    <AiFillHeart onClick={menuHandler} className="like_icons2" />
+                    <AiOutlineHeart
+                      onClick={menuHandler}
+                      className="like_icons1"
+                    />
+                    <AiFillHeart
+                      onClick={menuHandler}
+                      className="like_icons2"
+                    />
                   </i>
                 </div>
                 <div className="course-one__content">
                   <a href="#none" className="course-one__category">
-                    {courseCategory}
+                    {card.tag}
                   </a>
                   <div className="course-one__admin">
-                    <img src={img} alt="" />
-                    by <Link to="/teachersdetalis">{byName}</Link>
+                    <img src={card.img} alt="" />
+                    by <Link to="/teachersdetalis">{card.author}</Link>
                   </div>
                   <h2 className="course-one__title">
-                    <Link to="/coursedetalis">{courseDetals}</Link>
+                    <Link to="/coursedetalis">{card.title}</Link>
                   </h2>
-                  
+
                   <div className="course-one__meta">
                     <a href="/course-details">
-                      <i className="far fa-clock"></i> {hours}
+                      <i className="far fa-clock"></i> {card.hours}
                     </a>
                     <a href="/course-details">
-                      <i className="far fa-folder-open"></i> {lectures}
+                      <i className="far fa-folder-open"></i> {card.lectures}
                     </a>
-                    <a href="/course-details">{price}</a>
+                    <a href="/course-details">{card.price}</a>
                   </div>
                   <a href="#none" className="course-one__link">
                     See Preview
@@ -87,15 +88,10 @@ const courseCards =  [
                 </div>
               </div>
             </div>
-
-            })}
-
-            
-          
-
-          </div>
+          )})}
         </div>
-    </section>      
+      </div>
+    </section>
     // <section className="course-one course-page">
     //   <div className="container">
     //     <div className="row">
@@ -119,7 +115,7 @@ const courseCards =  [
     //             <h2 className="course-one__title">
     //               <Link to="/coursedetalis">New react bootcamp</Link>
     //             </h2>
-                
+
     //             <div className="course-one__meta">
     //               <a href="/course-details">
     //                 <i className="far fa-clock"></i> 10 Hours
@@ -333,4 +329,3 @@ const courseCards =  [
 };
 
 export default Courses;
-
